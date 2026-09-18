5c3d335568f2597c44599fcd439cde75229f7232
Ruturajpowar12 <ruturajpowar2038@gmail.com>
2026-09-18 18:44:56 +0530

    selection Sort program

---
+++ Dsa-lab/src/sort/SelectionSort.java
@@ -0,0 +1,56 @@
+package sort;
+
+import java.util.Scanner;
+
+public class SelectionSort {
+	
+	public static void selectionSort(int[] arr, int n) {
+		
+		for(int i= 0; i < n - 1; i++) {
+			
+			int minIndex = i;
+			
+			for(int j = i + 1; j < n ;j++) {
+				if(arr[j] < arr[minIndex]) {
+					minIndex = j;
+				}	
+			}
+			
+			int temp = arr[i];
+			arr[i]= arr[minIndex];
+			arr[minIndex] = temp;
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the Array Size :");
+		int n = sc.nextInt();
+		
+		int[] arr = new int[n];
+		
+		System.out.print("Enter Array Elements :");
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();		
+			
+		}
+		
+		System.out.print("\n Original array :");
+		for(int i=0;i<n;i++) {
+			System.out.print(arr[i] + " ");		
+			
+		}
+		
+		selectionSort(arr,n);
+		
+		System.out.print("\n Sorted array :");
+		for(int i=0;i<n;i++) {
+			System.out.print(arr[i] + " ");		
+			
+		}
+
+		sc.close();
+	}
+
+}
