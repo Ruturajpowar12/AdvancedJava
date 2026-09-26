59f82c959ad289b4665797755a71fc34a0c72cd3
Ruturajpowar12 <ruturajpowar2038@gmail.com>
2026-09-26 11:58:14 +0530

    An e-commerce application has a method that receives a list of products and only needs to display them. The list may contain Book, Mobile, Laptop, or other product objects. Use List<?>

---
+++ AdvanceJava_Programs/src/wildcardRealLife/ECommerceProductCatalog.java
@@ -0,0 +1,33 @@
+package wildcardRealLife;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//An e-commerce application has a method that receives a list of products 
+//and only needs to display them. The list may contain Book, Mobile, Laptop, or 
+//other product objects. Use List<?>.
+
+
+class Product { 
+	String name; 
+	Product(String name){
+		this.name = name; 
+		} 
+	public String toString(){
+		return name; 
+		}
+	}
+
+public class ECommerceProductCatalog {
+	    public static void displayProducts(List<?> products) {
+	        for (Object p : products) {
+	        	System.out.println(p);
+	        }
+	    }
+
+	    public static void main(String[] args) {
+	        List<Product> products = Arrays.asList(new Product("Book"), new Product("Mobile"));
+	        displayProducts(products);
+	    }
+	}
